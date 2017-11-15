ac586b6e894c1c8f1d8be473d2ab1de565614611
Tobi Beernaert <tobi@magier.be>
2017-11-16 00:01:31 +0100

    Remove the obsolete twitter cli options
    They were optional anyway ...
    
    Signed-off-by: Tobi Beernaert <tobi@magier.be>

--- src/Command/RaffleCommand.php
+++ src/Command/RaffleCommand.php
@@ -24,10 +24,6 @@ class RaffleCommand extends Command
             ->setDescription('Pick a random name from a list')
             ->addOption('meetup-id', null, InputOption::VALUE_REQUIRED)
             ->addOption('joindin-id', null, InputOption::VALUE_REQUIRED)
-            ->addOption('twitter-consumer-key', null, InputOption::VALUE_OPTIONAL)
-            ->addOption('twitter-consumer-secret', null, InputOption::VALUE_OPTIONAL)
-            ->addOption('twitter-oauth-token', null, InputOption::VALUE_OPTIONAL)
-            ->addOption('twitter-oauth-secret', null, InputOption::VALUE_OPTIONAL)
         ;
     }
 
