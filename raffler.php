120a84da95a3d666e6be49d0a5d7a1c856d898c6
Tobi Beernaert <tobi@magier.be>
2017-11-15 23:58:48 +0100

    Load .env from root directory
    Signed-off-by: Tobi Beernaert <tobi@magier.be>

--- raffler.php
+++ raffler.php
@@ -4,6 +4,13 @@ require __DIR__ . '/vendor/autoload.php';
 
 use PhpWvl\Command\RaffleCommand;
 use Symfony\Component\Console\Application;
+use Dotenv\Dotenv;
+
+try {
+    (new Dotenv(__DIR__))->load();
+} catch (Exception $exception) {
+    // meh
+}
 
 $application = new Application();
 $application->add(new RaffleCommand());
