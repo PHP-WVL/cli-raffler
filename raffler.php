d1d932002eed879d12de692c98964d67c6f5b820
BlackEagle <ike.devolder@gmail.com>
2019-06-19 21:56:19 +0200

    remove usage of dotenv in raffler
    since we only retrieved twitter information from the .env file we can
    now remove the usage of Dotenv\Dotenv.
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- raffler.php
+++ raffler.php
@@ -4,14 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use PhpWvl\Command\RaffleCommand;
 use Symfony\Component\Console\Application;
-use Dotenv\Dotenv;
-
-try {
-    (new Dotenv(__DIR__))->load();
-} catch (Exception $exception) {
-    echo "Could not load .env file, check if your configuration is correct";
-    exit();
-}
 
 $application = new Application();
 $application->add(new RaffleCommand());
